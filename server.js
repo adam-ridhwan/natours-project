e4420f6c167b83623f3a35ba6ac7315a14dcf380
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-01-24 16:09:40 -0500

    handling uncaught exceptions

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('\x1b[31m', '❌ UNCAUGHT EXCEPTION! Shutting down...', '\x1b[0m');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -20,9 +27,19 @@ mongoose
     console.log('\x1b[32m', '✅ Database connection successful!', '\x1b[0m')
   );
 
-/* ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ */
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('\x1b[36m', `⚙️  App running on port ${port}...`, '\x1b[0m');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(
+    '\x1b[31m',
+    '❌ UNHANDLED REJECTION! Shutting down...',
+    '\x1b[0m'
+  );
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
