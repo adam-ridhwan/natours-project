b7df5588dcf28bff80ef8d7ec9a3c100bddaeac4
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-01-24 15:54:02 -0500

    fixed error controllers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,6 @@
 const AppError = require('../utils/appError');
 
+// HANDLE DATABASE ERRORS
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
@@ -10,6 +11,14 @@ const handleDuplicateFieldsDB = (err) => {
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+/* ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ */
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -48,10 +57,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
+    let error = err;
 
-    if (error.name === 'CastError') error = handleCastErrorDB(err);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
